07cd8986bb512f502306ee96b2856c9d6c7d22a9
n.kovalev <n.kovalev@npo-echelon.ru>
2024-12-13 11:55:45 +0300

    added lost file

---
+++ internal/services/generic_services.go
@@ -0,0 +1,35 @@
+package services
+
+import (
+	rep "TaskManager/internal/repository"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type GenericService[T any] struct {
+	Repository *rep.GenericRepository[T]
+}
+
+func NewGenericService[T any](db *gorm.DB) *GenericService[T] {
+	return &GenericService[T]{Repository: rep.NewGenericRepository[T](db)}
+}
+
+func (serv GenericService[T]) GetByID(id int64) (model *T, err error) {
+	model, err = serv.Repository.FindByID(id)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка поиске по id: %s", err)
+	}
+	if model == nil {
+		return nil, fmt.Errorf("нет записи с таким ID")
+	}
+	return model, nil
+}
+
+func (serv GenericService[T]) Delete(id int64) error {
+	err := serv.Repository.Delete(id)
+	if err != nil {
+		return fmt.Errorf("Ошибка при удалении записи: %w", err)
+	}
+	return nil
+}
